59ef061367cafcb4ebcb6271fb71ca33446be2b0
Nate Lentz <nathanalentz@gmail.com>
2022-03-09 23:31:03 -0500

    feat(ui): Add Vue 3 proj with typescript and tailwindcss

---
+++ client/src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold underline">{{ msg }}</h1>
+
+  <button
+    type="button"
+    class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded my-2"
+    @click="count++"
+  >
+    count is: {{ count }}
+  </button>
+  <p>
+    Edit
+    <code>components/HelloWorld.vue</code> to test hot module replacement.
+  </p>
+</template>
+
+<style scoped></style>
