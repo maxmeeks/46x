99b8df79d5c73a92d0ccc09b37e9d0e49d6dc180
Max Meekhoff <maxwellm@c2experience.com>
2022-03-11 08:41:42 -0500

    add vue csv import

--- web/src/views/Dashboard.vue
+++ web/src/views/Dashboard.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+  import { ref } from 'vue'
+  import {VueCsvImportPlugin} from "vue-csv-import"
 </script>
 
 <template>
   <h1 class="text-3xl font-bold">Dashboard Works</h1>
+
+  <vue-csv-import>
+      <vue-csv-input name="file"></vue-csv-input>
+  </vue-csv-import>
 </template>
 
 <style scoped></style>
