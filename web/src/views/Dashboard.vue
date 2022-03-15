b27f4f860d010ec1e96325517b090f80608eca2a
Max Meekhoff <meekhoffm@gmail.com>
2022-03-14 22:19:06 -0400

    commenting out plugin

--- web/src/views/Dashboard.vue
+++ web/src/views/Dashboard.vue
@@ -1,14 +1,21 @@
 <script setup lang="ts">
   import { ref } from 'vue'
-  import {VueCsvImportPlugin} from "vue-csv-import"
+  // import {VueCsvImportPlugin} from "vue-csv-import"
 </script>
 
 <template>
   <h1 class="text-3xl font-bold">Dashboard Works</h1>
 
-  <vue-csv-import>
-      <vue-csv-input name="file"></vue-csv-input>
-  </vue-csv-import>
+  <!-- <vue-csv-import
+    v-slot="{file}"
+    v-model="csv"
+    :fields="{name: {required: false, label: 'Name'}, age: {required: true, label: 'Age'}}"
+  >
+    <vue-csv-toggle-headers></vue-csv-toggle-headers>
+    <vue-csv-errors></vue-csv-errors>
+    <vue-csv-input></vue-csv-input>
+    <vue-csv-map :auto-match="false"></vue-csv-map>
+  </vue-csv-import> -->
 </template>
 
 <style scoped></style>
--- web/src/views/Table.vue
+++ web/src/views/Table.vue
@@ -8,18 +8,13 @@ const count = ref(0)
 
 <template>
   <h1 class="text-3xl font-bold underline">{{ msg }}</h1>
-
   <button
     type="button"
     class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded my-2"
     @click="count++"
   >
-    count is: {{ count }}
+    count is: {{ count}}
   </button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
 </template>
 
 <style scoped></style>
